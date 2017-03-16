0399d19f58f1db853759337b7e16d7fba00fe4c9
Alex Vanderbist <alex.vanderbist@gmail.com>
2017-03-16 09:59:48 +0100

    Added migration to enable foreign key constraints

--- laravel/database/migrations/2017_03_02_000000_enable_foreign_keys.php
+++ laravel/database/migrations/2017_03_02_000000_enable_foreign_keys.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,7 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token')->index();
-            $table->timestamp('created_at')->nullable();
-        });
+        Schema::enableForeignKeyConstraints();
     }
 
     /**
@@ -27,6 +23,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        //
     }
 }
